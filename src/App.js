754c08ed2380984d74e63656f502a07c7ca29279
Camilo Agudelo <jcagudelo42@misena.edu.co>
2021-09-18 11:07:09 -0500

    CSS en React - 6/22

--- src/App.js
+++ src/App.js
@@ -7,18 +7,10 @@ import { TodoItem } from "./TodoItem";
 import { TodoList } from "./TodoList";
 
 const todos = [
-  {
-    text: "Cortar cebolla",
-    completed: false,
-  },
-  {
-    text: "Tomar el curso de intro a React",
-    completed: false,
-  },
-  {
-    text: "Llorar con la llorona",
-    completed: false,
-  },
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el cursso de intro a React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+  { text: "LALALALAA", completed: false },
 ];
 
 function App() {
@@ -30,7 +22,11 @@ function App() {
 
       <TodoList>
         {todos.map((todo) => (
-          <TodoItem text={todo.text} />
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
 
